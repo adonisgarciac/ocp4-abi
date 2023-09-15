971944c035bfeb97a6e50e761aa07e9ef251ea6e
Jorge Tudela <jtudelag@redhat.com>
2023-09-15 10:42:39 -0400

    Make ee build more verbose

--- ansible/execution_environment/build_ee.sh
+++ ansible/execution_environment/build_ee.sh
@@ -5,4 +5,4 @@ EE_IMAGE_NAME="ee-ocp-vmware:latest"
 
 #Build it
 subscription-manager repos --enable=rhocp-4.12-for-rhel-8-x86_64-rpms
-ansible-builder build --tag "$EE_IMAGE_NAME"
+ansible-builder -v 3 build --tag "$EE_IMAGE_NAME"
