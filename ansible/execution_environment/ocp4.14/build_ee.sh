d5bad692669a2911ef0ee7facca4a97f4ea4a960
Jorge Tudela <jtudelag@redhat.com>
2024-02-08 04:50:45 -0500

    Bump dependencies, parametrize base image

--- ansible/execution_environment/ocp4.14/build_ee.sh
+++ ansible/execution_environment/ocp4.14/build_ee.sh
@@ -17,8 +17,8 @@ EE_IMAGE_NAME="ee-ocp-install"
 OCP_MAJOR=4
 OCP_MINOR=14
 OCP_PATCH=10
-COREOS_INSTALLER_VERSION="0.17.0-1"
-BUTANE_VERSION="0.18.0-1"
+COREOS_INSTALLER_VERSION="0.17.0-3"
+BUTANE_VERSION="0.19.0"
 #Used for RPMs repos.
 RHEL_VERSION="9"
 
@@ -88,12 +88,12 @@ build () {
 
   #Build it
   ansible-builder build -v 3 \
-	--build-arg EE_BASE_IMAGE="registry.redhat.io/ansible-automation-platform-24/ee-minimal-rhel${RHEL_VERSION}:latest" \
-  	--build-arg "RHEL_VERSION=$RHEL_VERSION" \
+   --build-arg EE_BASE_IMAGE="registry.redhat.io/ansible-automation-platform-24/ee-minimal-rhel${RHEL_VERSION}:latest" \
+   --build-arg "RHEL_VERSION=$RHEL_VERSION" \
    --build-arg "OCP_XY=$OCP_XY" \
    --prune-images \
    --squash all \
-  	--tag "${EE_IMAGE_NAME}:${OCP_XYZ}"
+   --tag "${EE_IMAGE_NAME}:${OCP_XYZ}"
 }
 
 all () {
